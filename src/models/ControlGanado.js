b76cb83a29bc4715ab24696232dae964e8bda274
Gonzalo Baez <gonzaasd7@gmail.com>
2025-02-17 16:49:37 -0300

    pruebas

--- src/models/ControlGanado.js
+++ src/models/ControlGanado.js
@@ -4,22 +4,6 @@ module.exports = (sequelize) => {
     sequelize.define(
         "ControlGanado",
         {
-            ganadoId: {
-                type: DataTypes.UUID,
-                references: {
-                    model: "Ganados", // Nombre de la tabla Ganado
-                    key: "id", // Clave primaria de Ganado
-                },
-                allowNull: false,
-            },
-            controlVeterinarioId: {
-                type: DataTypes.UUID,
-                references: {
-                    model: "ControlVeterinarios", // Nombre de la tabla ControlVeterinario
-                    key: "id", // Clave primaria de ControlVeterinario
-                },
-                allowNull: false,
-            },
             fecha: {
                 type: DataTypes.DATE,
                 allowNull: true,
