cbb4775ea059ad4fae5836b99008d8defb87a68c
Gonzalo Baez <gonzaasd7@gmail.com>
2025-02-17 16:31:52 -0300

    modelo cambiado

--- src/models/ControlGanado.js
+++ src/models/ControlGanado.js
@@ -4,6 +4,22 @@ module.exports = (sequelize) => {
     sequelize.define(
         "ControlGanado",
         {
+            ganadoId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "Ganados", // Nombre de la tabla Ganado
+                    key: "id", // Clave primaria de Ganado
+                },
+                allowNull: false,
+            },
+            controlVeterinarioId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "ControlVeterinarios", // Nombre de la tabla ControlVeterinario
+                    key: "id", // Clave primaria de ControlVeterinario
+                },
+                allowNull: false,
+            },
             fecha: {
                 type: DataTypes.DATE,
                 allowNull: true,
