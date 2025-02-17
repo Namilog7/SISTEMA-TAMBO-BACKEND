14440c8136f95c790cd19ec91f99bd48993bff0f
Gonzalo Baez <gonzaasd7@gmail.com>
2025-02-17 15:35:23 -0300

    cambios en getcontrol

--- src/handlers/tambo/controlVeterinario/getControlVeterinario.js
+++ src/handlers/tambo/controlVeterinario/getControlVeterinario.js
@@ -2,12 +2,17 @@ const { ControlVeterinario } = require("../../../db");
 
 const getControlVeterinario = async (req, res) => {
     try {
-        const response = await ControlVeterinario.findAll()
-        return res.json(response)
+        const response = await ControlVeterinario.findAll();
+        const caravanas = response.map(control => control.caravana);
+        return res.json({
+            response,
+            caravanas
+        });
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(500).json({ message: "Error al obtener los controles" });
     }
 }
 
-module.exports = getControlVeterinario
+module.exports = getControlVeterinario;
