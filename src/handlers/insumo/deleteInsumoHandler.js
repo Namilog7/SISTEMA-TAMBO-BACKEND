7e6b1b14d40b10199e72bada6bde70c11fb6f7cd
Gonzalo Baez <gonzaasd7@gmail.com>
2025-02-17 16:05:35 -0300

    cambio en delete Insumo a query

--- src/handlers/insumo/deleteInsumoHandler.js
+++ src/handlers/insumo/deleteInsumoHandler.js
@@ -2,7 +2,7 @@ const deleteInsumo = require("../../controllers/insumo/deleteInsumo")
 
 
 const deleteInsumoHandler = async (req, res) => {
-    const { id_sector, id } = req.body
+    const { id_sector, id } = req.query
     try {
         const deleteOne = await deleteInsumo({ id_sector, id })
         res.json(deleteOne)
