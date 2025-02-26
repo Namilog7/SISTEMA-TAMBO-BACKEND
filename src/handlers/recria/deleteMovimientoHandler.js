0f47cdd23167634a367a0791339b9c40df3865f9
KJoaquinklein <joa_klein1@hotmail.com>
2025-02-26 09:53:35 -0300

    correciones en movimientos

--- src/handlers/recria/deleteMovimientoHandler.js
+++ src/handlers/recria/deleteMovimientoHandler.js
@@ -1,18 +1,29 @@
-const { Movimiento_anotacion } = require("../../db");
-const crudController = require("../../controllers/crudController")
+const { Movimiento_anotacion, Macho } = require("../../db");
+const crudController = require("../../controllers/crudController");
 
 const deleteMovimientoHandler = async (req, res) => {
-    const { id } = req.params
-    const deleteMovimiento = crudController(Movimiento_anotacion)
+    const { id } = req.params;
+    const deleteMovimiento = crudController(Movimiento_anotacion);
+    let terneros = await Macho.findOne();
     try {
-        const result = await deleteMovimiento.delete(id)
-        return res.json({
-            result
-        })
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({ error: error.message })
-    }
-}
+        // modificar contador
+        const movimiento = await deleteMovimiento.readOne(id);
+        if (movimiento.tipo_movimiento === "INGRESO") {
+            terneros.ternero_contador = Number(terneros.ternero_contador) - Number(ternero_contador);
+        } else if (movimiento.tipo_movimiento === "BAJA") {
+            terneros.ternero_contador = Number(terneros.ternero_contador) + Number(ternero_contador);
+        }
+        await terneros.save();
 
-module.exports = deleteMovimientoHandler
+        // eliminar
+        const result = await deleteMovimiento.delete(id);
+        return res.json({
+            result,
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = deleteMovimientoHandler;
