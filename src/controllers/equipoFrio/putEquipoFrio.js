eacea6be4f9d4c5331bfb3b48e32594a89b8515d
Gonzalo Baez <gonzaasd7@gmail.com>
2025-03-20 11:45:50 -0300

    fix pago nota router

--- src/controllers/equipoFrio/putEquipoFrio.js
+++ src/controllers/equipoFrio/putEquipoFrio.js
@@ -1,5 +1,7 @@
 const { EquipoFrio } = require("../../db");
 
 const putEquipoFrio = async ({ nombre, litros, capacidad }) => {
-    const equipo = await EquipoFrio.findOne({ where: { nombre } })
+    const equipo = await EquipoFrio.findOne({ where: { nombre } });
+    equipo.litros += litros,
+        equipo.capacidad = capacidad
 }
