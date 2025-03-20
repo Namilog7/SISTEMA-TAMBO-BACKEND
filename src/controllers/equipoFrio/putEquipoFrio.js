2c18e945e1a8251b6c538625bcd89546aec8624b
Gonzalo Baez <gonzaasd7@gmail.com>
2025-03-20 11:26:06 -0300

    fix pagoRouter y notaRouter

---
+++ src/controllers/equipoFrio/putEquipoFrio.js
@@ -0,0 +1,5 @@
+const { EquipoFrio } = require("../../db");
+
+const putEquipoFrio = async ({ nombre, litros, capacidad }) => {
+    const equipo = await EquipoFrio.findOne({ where: { nombre } })
+}
