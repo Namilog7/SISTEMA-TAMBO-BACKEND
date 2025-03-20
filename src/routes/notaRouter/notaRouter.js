3cbcc9e7dd379019645bf87b49ba4cb55b65d7e4
Gonzalo Baez <gonzaasd7@gmail.com>
2025-03-20 10:56:11 -0300

    cambio en notas y pagos

--- src/routes/notaRouter/notaRouter.js
+++ src/routes/notaRouter/notaRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const postNotaHandler = require("../../handlers/nota/postNotaHandler");
 const getNotaHandler = require("../../handlers/nota/getNotaHandler");
+const getNotasHandler = require("../../handlers/nota/getNotasHandler");
 
 const notaRouter = Router();
-notaRouter.get("/:id_afectado", getNotaHandler)
+notaRouter.get("/:id_afectado", getNotaHandler);
+notaRouter.get("/", getNotasHandler)
 notaRouter.post("/", postNotaHandler)
 
 module.exports = notaRouter
--- src/routes/pagoRouter/pagoRouter.js
+++ src/routes/pagoRouter/pagoRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const getPagoHandler = require("../../handlers/pago/getPagoHandler");
 const postPagoHandler = require("../../handlers/pago/postPagoHandler");
+const getPagosHandler = require("./getPagosHandler");
 
 const pagoRouter = Router();
 pagoRouter.get("/:id", getPagoHandler);
+pagoRouter.get("/", getPagosHandler)
 pagoRouter.post("/", postPagoHandler)
 
 
