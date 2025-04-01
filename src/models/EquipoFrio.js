8c92448c2a2d941dc0a74cdc0d10f86a3d38b06f
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-01 17:35:57 -0300

    fix equipoFrio

--- src/models/EquipoFrio.js
+++ src/models/EquipoFrio.js
@@ -15,22 +15,10 @@ module.exports = (sequelize) => {
             },
             litros: {
                 type: DataTypes.FLOAT,
-                allowNull: true,
-                validate: {
-                    min: { args: [0], msg: "Los litros no pueden ser negativos." },
-                    litrosNoMayorCapacidad(value) {
-                        if (value !== null && this.capacidad !== null && value > this.capacidad) {
-                            throw new Error("Los litros no pueden ser mayores a la capacidad.");
-                        }
-                    }
-                }
             },
             capacidad: {
                 type: DataTypes.FLOAT,
                 allowNull: true,
-                validate: {
-                    min: { args: [0], msg: "La capacidad no puede ser negativa." }
-                }
             }
         },
         { timestamps: true }
