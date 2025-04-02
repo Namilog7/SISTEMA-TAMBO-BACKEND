a9618e9544591d2839978974c3f778ec76bbf967
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-02 10:48:28 -0300

    fix cambioLitros

--- src/controllers/equipoFrio/cambioLitros.js
+++ src/controllers/equipoFrio/cambioLitros.js
@@ -1,24 +1,32 @@
 const { EquipoFrio } = require("../../db");
 
 const cambioLitros = async ({ litros }, transaction) => {
+    if (litros <= 0) throw new Error("La cantidad de litros debe ser positiva");
+
     const equipoTambo = await EquipoFrio.findOne({
-        where: {
-            nombre: "Tambo"
-        }
-    }, { transaction })
+        where: { nombre: "Tambo" },
+        transaction
+    });
+
     const equipoFabrica = await EquipoFrio.findOne({
-        where: {
-            nombre: "Fabrica"
-        }
-    }, { transaction })
-    if (!equipoTambo) throw new Error("No existe el equipo Tambo")
-    if (!equipoFabrica) throw new Error("No existe el equipo Fabrica")
-    equipoFabrica.litros += Number(litros)
-    equipoTambo.litros -= Number(litros)
+        where: { nombre: "Fabrica" },
+        transaction
+    });
 
-    equipoFabrica.save({ transaction })
-    equipoTambo.save({ transaction })
-    return
-}
+    if (!equipoTambo) throw new Error("No existe el equipo Tambo");
+    if (!equipoFabrica) throw new Error("No existe el equipo Fabrica");
 
-module.exports = cambioLitros
+    if (equipoTambo.litros < litros) {
+        throw new Error("No hay suficientes litros en el equipo Tambo");
+    }
+
+    equipoFabrica.litros += Number(litros);
+    equipoTambo.litros -= Number(litros);
+
+    await equipoFabrica.save({ transaction });
+    await equipoTambo.save({ transaction });
+
+    return;
+};
+
+module.exports = cambioLitros;
