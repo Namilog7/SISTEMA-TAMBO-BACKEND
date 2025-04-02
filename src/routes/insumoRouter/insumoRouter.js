f2a7574f83287204988212221ce2c2856ab5323e
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-02 13:24:00 -0300

    add getComprobanteHandler en router

--- src/routes/insumoRouter/insumoRouter.js
+++ src/routes/insumoRouter/insumoRouter.js
@@ -7,6 +7,7 @@ const putInsumoHandler = require("../../handlers/insumo/putInsumoHandler");
 const deleteInsumoHandler = require("../../handlers/insumo/deleteInsumoHandler");
 const getAllInsumosHandler = require("../../handlers/insumo/returnUuidsInsumoHandler");
 const postComprobanteHandler = require("../../handlers/insumo/postComprobanteHandler");
+const getComprobanteHandler = require("../../handlers/insumo/getComprobanteHandler");
 
 const insumoRouter = Router();
 insumoRouter.get("/", getAllInsumosHandler)
@@ -14,6 +15,7 @@ insumoRouter.get("/:id_sector", getInsumoHandler);
 insumoRouter.post("/", modelValidateMid(insumoValidationModel), postInsumoHandler) // cambio en la implementacion
 insumoRouter.put("/", modelValidateMid(insumoValidationModel), putInsumoHandler) //queda modificar el put
 insumoRouter.post("/comprobante", postComprobanteHandler)
+insumoRouter.get("/comprobante", getComprobanteHandler)
 insumoRouter.delete("/", deleteInsumoHandler)
 
 
