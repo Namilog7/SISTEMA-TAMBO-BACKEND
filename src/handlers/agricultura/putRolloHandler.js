d0c5ac62fd5a4a823346d83185916bd164ce4800
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-07 14:51:38 -0300

    fix putRollo

--- src/handlers/agricultura/putRolloHandler.js
+++ src/handlers/agricultura/putRolloHandler.js
@@ -2,11 +2,11 @@ const crudController = require("../../controllers/crudController");
 const { Rollo } = require("../../db");
 
 const putRolloHandler = async (req, res) => {
-    const { nombre, cantidad, precio, id } = req.body
+    const { tipo, cantidad, precio, id } = req.body
     const rollos = crudController(Rollo);
 
     try {
-        await rollos.update({ nombre, cantidad, precio, id })
+        await rollos.update({ tipo, cantidad, precio, id })
         res.json({
             message: "Datos actualizados"
         })
