15477ad51b72eb1ac8633c54ab0937dc45788c23
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-22 20:09:57 -0300

    fix postMesesPagoHandler

--- src/handlers/casa/postMesesPagoHandler.js
+++ src/handlers/casa/postMesesPagoHandler.js
@@ -1,12 +1,12 @@
-const { MesesPago } = require("../../db");
+const { MesesCompromiso } = require("../../db");
 const crudController = require("../../controllers/crudController");
 
 const postMesesPagoHandler = async (req, res) => {
-    const { id_compromiso, monto_pagado, fecha } = req.body
-    const postMesesPago = crudController(MesesPago);
+    const { id_compromiso, monto, fecha } = req.body
+    const postMesesPago = crudController(MesesCompromiso);
     try {
-        const nuevoPago = await postMesesPago.create({ id_compromiso, monto_pagado, fecha })
-        req.json(nuevoPago)
+        const nuevoPago = await postMesesPago.create({ id_compromiso, monto, fecha, estado: "PAGADO" })
+        res.json(nuevoPago)
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: error.message })
