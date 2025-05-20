9b821f49ce625225828d0714b9441f03be751b9f
KJoaquinklein <joa_klein1@hotmail.com>
2025-05-20 20:26:59 -0300

    correcciones de proveedores - transaction v2

--- src/handlers/tambo/retiroLeche/getRetiroByIdHandler.js
+++ src/handlers/tambo/retiroLeche/getRetiroByIdHandler.js
@@ -1,15 +1,14 @@
-const RetiroLeche = require("../../../db");
-
+const { RetiroLeche } = require("../../../db");
 
 const getRetiroByIdHandler = async (req, res) => {
     const { id } = req.params;
     try {
         const retiros = await RetiroLeche.findAll({
-            where: { id_cliente: id }
-        })
-        res.json({ retiros })
+            where: { id_cliente: id },
+        });
+        res.json({ retiros });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
-module.exports = getRetiroByIdHandler
+};
+module.exports = getRetiroByIdHandler;
--- src/routes/tamboRouter/retiroLecheRouter/retiroLecheRouter.js
+++ src/routes/tamboRouter/retiroLecheRouter/retiroLecheRouter.js
@@ -3,17 +3,20 @@ const postRetiroLecheHandler = require("../../../handlers/tambo/retiroLeche/post
 const getRetiroLecheHandler = require("../../../handlers/tambo/retiroLeche/getRetiroLecheHandler");
 const getPartialRetiroLecheHandler = require("../../../handlers/tambo/retiroLeche/getPartialRetiroLecheHandler");
 const putRetiroLecheHandler = require("../../../handlers/tambo/retiroLeche/putRetiroLecheHandler");
-const modelValidateMid = require("../../../middlewares/modelValidateMid")
-const { postRetiroLecheModel, retiroLecheUpdateModel } = require("../../../middlewares/tambo/retiroLeche/postRetiroLecheMid");
+const modelValidateMid = require("../../../middlewares/modelValidateMid");
+const {
+    postRetiroLecheModel,
+    retiroLecheUpdateModel,
+} = require("../../../middlewares/tambo/retiroLeche/postRetiroLecheMid");
 const liquidacionRouter = require("../../liquidacion/liquidacionRouter");
 const getRetiroByIdHandler = require("../../../handlers/tambo/retiroLeche/getRetiroByIdHandler");
 
 const retiroLecheRouter = Router();
 retiroLecheRouter.get("/", getRetiroLecheHandler);
-retiroLecheRouter.get("/:id", getRetiroByIdHandler)
-retiroLecheRouter.get("/parcial", getPartialRetiroLecheHandler)
+// retiroLecheRouter.get("/:id", getRetiroByIdHandler)
+retiroLecheRouter.get("/parcial", getPartialRetiroLecheHandler);
 retiroLecheRouter.post("/", modelValidateMid(postRetiroLecheModel), postRetiroLecheHandler);
 retiroLecheRouter.put("/", modelValidateMid(retiroLecheUpdateModel), putRetiroLecheHandler);
-retiroLecheRouter.use("/liquidacion", liquidacionRouter)
+retiroLecheRouter.use("/liquidacion", liquidacionRouter);
 
-module.exports = retiroLecheRouter
+module.exports = retiroLecheRouter;
