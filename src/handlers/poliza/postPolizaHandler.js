c7436ae5085bb26a9c6035b340e9178c2d010ff0
KJoaquinklein <joa_klein1@hotmail.com>
2025-05-21 14:49:44 -0300

    correcciones 21-5

--- src/handlers/poliza/postPolizaHandler.js
+++ src/handlers/poliza/postPolizaHandler.js
@@ -1,21 +1,39 @@
-const { PolizaSeguro } = require("../../db");
+const { PolizaSeguro, conn } = require("../../db");
 const crudController = require("../../controllers/crudController");
 const postCloudinary = require("../../controllers/postCloudinary");
 
 const postPolizaHandler = async (req, res) => {
-    const { numero_poliza, nombre, seccion, desde, hasta, importe, estado, cantidad_cuotas, factura_base64, afectados } = req.body
+    const transaction = await conn.transaction();
+    const {
+        numero_poliza,
+        nombre,
+        seccion,
+        desde,
+        hasta,
+        importe,
+        estado,
+        cantidad_cuotas,
+        factura_base64,
+        afectados,
+    } = req.body;
     const postPoliza = crudController(PolizaSeguro);
     try {
         let foto_factura;
         if (factura_base64) {
             foto_factura = await postCloudinary(factura_base64, "poliza_facturas");
         }
-        const poliza = await postPoliza.create({ numero_poliza, nombre, seccion, desde, hasta, importe, estado, cantidad_cuotas, foto_factura, afectados })
-        res.json(poliza)
+        const poliza = await postPoliza.create(
+            { numero_poliza, nombre, seccion, desde, hasta, importe, estado, cantidad_cuotas, foto_factura, afectados },
+            { transaction }
+        );
+
+        await transaction.commit();
+
+        res.json(poliza);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: error.message })
+        res.status(500).json({ error: error.message });
     }
-}
+};
 
-module.exports = postPolizaHandler
+module.exports = postPolizaHandler;
