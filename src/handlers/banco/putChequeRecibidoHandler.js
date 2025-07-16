2d3bc11826067eab5e16fe82531b16e8bc9d9e1b
AlejandroAgueroDev <alejandroaguerodev@gmail.com>
2025-07-16 00:37:35 -0300

    correcciones finales

--- src/handlers/banco/putChequeRecibidoHandler.js
+++ src/handlers/banco/putChequeRecibidoHandler.js
@@ -2,15 +2,37 @@ const { ChequeRecibido } = require("../../db");
 const crudController = require("../../controllers/crudController");
 
 const putChequeRecibidoHandler = async (req, res) => {
-    const { id, detalle, estado } = req.body
-    const postCheque = crudController(ChequeRecibido);
-    try {
-        const putCheque = await postCheque.update({ id, detalle, estado })
-        res.json(putCheque)
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message })
-    }
-}
+  const {
+    id,
+    banco,
+    origen,
+    numero_cheque,
+    fecha_emision,
+    fecha_pago,
+    importe,
+    destino,
+    estado,  
+  } = req.body;
 
-module.exports = putChequeRecibidoHandler
+  const postCheque = crudController(ChequeRecibido);
+  try {
+    const putCheque = await postCheque.update({
+      id,
+      banco,
+      origen,
+      numero_cheque,
+      fecha_emision,
+      fecha_pago,
+      importe,
+      destino,
+      estado, 
+    });
+    res.json(putCheque);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+module.exports = putChequeRecibidoHandler;
