03df76c672364cf9c976aa2f6a00858b1929b31c
Gonzalo Baez <gonzaasd7@gmail.com>
2025-07-19 14:37:35 -0300

    se saco de src carpeta certs, error critico de seguridad

--- src/db.js
+++ src/db.js
@@ -4,17 +4,17 @@ const path = require("path");
 const { DB_DEPLOY, DB_DEV } = process.env;
 const pg = require("pg");
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DB_DEV, {
     logging: false,
     native: false,
     dialectModule: pg,
     dialect: "postgres",
-    dialectOptions: {
+    /* dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false,
         },
-    },
+    }, */
 });
 
 // Obtención del nombre del archivo actual
